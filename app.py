38a60e87e1e8a52976ce8a42bebbf6be7dace051
Brillante Wang <brillante.wang@gmail.com>
2025-08-24 14:59:16 +0800

    Have endpoint receive file

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 
 app = FastAPI()
-
-class Document(BaseModel):
-    document: bytes
-    source_file_name: str
 
 class GDriveParts(BaseModel):
     document_type: str
     g_drive_file_name: str
 
 @app.post('/documents/g_drive_parts')
-async def create_g_drive_parts(document: Document) -> GDriveParts:
+async def create_g_drive_parts(document: UploadFile) -> GDriveParts:
+    print(document.filename, 'filename')
+    print(document.content_type, 'content type')
+    # Hits classifier processor to get document type
+    # Based on the document type
+    # it sends that document to the corresponding extractor processor
+    # It constructs the g_drive_file_name based on the extracted text
+    # Returns the document_type and g_drive_file_name
+
     response = GDriveParts(document_type='dog', g_drive_file_name='k')
     return response
